a4901dc955ba30da7cf60e10d8cfa816127dbe4e
Sokos6 <will.sokolowski@gmail.com>
2019-12-13 01:37:54 -0600

    class conversion

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
+class App extends React.Component {
 
-  return <div>Hi There!</div>;
-};
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    );
+
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDOM.render(
   <App />,
