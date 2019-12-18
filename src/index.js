f298cde4ad9c8ab7682b434016a1c1871cbf6430
Sokos6 <will.sokolowski@gmail.com>
2019-12-17 23:04:17 -0600

    working mount

--- src/index.js
+++ src/index.js
@@ -6,22 +6,30 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      lat: null
+      lat: null,
+      errorMessage: '',
     };
-
-    window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({
-          lat: position.coords.latitude
-        });
-      },
-      err => console.log(err)
-    );
   }
+
+    componentDidMount() {
+      window.navigator.geolocation.getCurrentPosition(
+        position => this.setState({ lat: position.coords.latitude }),
+        err => this.setState({ errorMessage: err.message })
+      );
+    }
+
 
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+      if (this.state.errorMessage && !this.state.lat) {
+        return <div>Error: {this.state.errorMessage}</div>;
+      }
+      if(!this.state.errorMessage && this.state.lat) {
+        return <div>Latitude: {this.state.lat}</div>;
+      }
+
+      return <div>Loading!</div>;
   }
 }
+
 
 ReactDOM.render(<App />, document.querySelector("#root"));
